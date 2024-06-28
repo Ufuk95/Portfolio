67f128b353f6a102d814d928db6580831a72c850
Ufuk Özsahin <oezsahin95@icloud.com>
2024-06-28 14:21:50 +0200

    skill optic implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
 
+  skillImages: { src: string, name: string }[] = [
+    { src: 'img/skills/angular.png', name: 'Angular' },
+    { src: 'img/skills/typescript.png', name: 'Typescript' },
+    { src: 'img/skills/javascript.png', name: 'Javascript' },
+    { src: 'img/skills/html.png', name: 'HTML' },
+    { src: 'img/skills/firebase.png', name: 'Firebase' },
+    { src: 'img/skills/git.png', name: 'GIT' },
+    { src: 'img/skills/css.png', name: 'CSS' },
+    { src: 'img/skills/api.png', name: 'Rest-Api' },
+    { src: 'img/skills/scrum.png', name: 'Scrum' },
+    { src: 'img/skills/material.png', name: 'Material Design' }
+  ];
 }
