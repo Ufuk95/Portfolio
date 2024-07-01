4bb1040720418eb7f41cc4a86b4cdb63ee5a052e
Ufuk Özsahin <oezsahin95@icloud.com>
2024-07-01 14:57:35 +0200

    contact area design close to finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
