1ad670cc4e57abf6d48c389b9c9b87ed5139d0ac
Ufuk Özsahin <oezsahin95@icloud.com>
2024-07-03 11:08:18 +0200

    hover effect done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent],
+  imports: [RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
