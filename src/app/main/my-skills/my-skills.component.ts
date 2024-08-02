97b161ddb7b4d903c750089014b58aa7b0785fb7
Ufuk Özsahin <oezsahin95@icloud.com>
2024-08-02 16:15:01 +0200

    high quality imgs

--- src/app/main/my-skills/my-skills.component.ts
+++ src/app/main/my-skills/my-skills.component.ts
@@ -13,16 +13,16 @@ import { VisibleOnScrollDirective } from '../../visible-on-scroll.directive';
 export class MySkillsComponent {
 
   skillImages: { src: string, name: string }[] = [
-    { src: '/img/skills/angular.png', name: 'Angular' },
-    { src: '/img/skills/typescript.png', name: 'Typescript' },
-    { src: '/img/skills/javascript.png', name: 'Javascript' },
-    { src: '/img/skills/html.png', name: 'HTML' },
-    { src: '/img/skills/firebase.png', name: 'Firebase' },
-    { src: '/img/skills/git.png', name: 'GIT' },
-    { src: '/img/skills/css.png', name: 'CSS' },
-    { src: '/img/skills/api.png', name: 'Rest-Api' },
-    { src: '/img/skills/scrum.png', name: 'Scrum' },
-    { src: '/img/skills/material.png', name: 'Material design' }
+    { src: '/img/icons/angular-icon.svg', name: 'Angular' },
+    { src: '/img/icons/ts-icon.svg', name: 'Typescript' },
+    { src: '/img/icons/js-icon.svg', name: 'Javascript' },
+    { src: '/img/icons/html-icon.svg', name: 'HTML' },
+    { src: '/img/icons/firebase-icon.svg', name: 'Firebase' },
+    { src: '/img/icons/git-icon.svg', name: 'GIT' },
+    { src: '/img/icons/css-icon.svg', name: 'CSS' },
+    { src: '/img/icons/api-icon.svg', name: 'Rest-Api' },
+    { src: '/img/icons/scrum.svg', name: 'Scrum' },
+    { src: '/img/icons/materialDesign.svg', name: 'Material design' }
   ];
 
   constructor(private translationService: LanguageService) {}
