2a17bf410d7c5137c148aee25efec5456962421b
Ufuk Özsahin <oezsahin95@icloud.com>
2024-08-03 09:22:48 +0200

    pokedex is back

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -15,8 +15,9 @@ export class PortfolioComponent {
   projects: { imgsrc: string, name: string, environment: string, text: string }[]  = [
     { imgsrc: '/img/portfolio/joinPortfolio.png', name: 'Join', environment: 'Angular | TypeScript | HTML | CSS | Firebase', text: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'},
     { imgsrc: '/img/portfolio/PoloPortfolio.png', name: 'El Pollo Loco', environment: 'JavaScript | HTML | CSS', text: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.'},
-    { imgsrc: '/img/portfolio/SimpleCRM.png', name: 'Simple CRM', environment: 'Angular | Firebase', text: 'A very Simple Customer Relationship Management system working with CRUD functionality.'},
-    { imgsrc: '/img/portfolio/Pokedex.png', name: 'Pokédex', environment: 'JavaScript | HTML | CSS | Api', text: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.'}
+    { imgsrc: '/img/portfolio/Pokedex.png', name: 'Pokédex', environment: 'JavaScript | HTML | CSS | Api', text: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.'},
+    { imgsrc: '/img/portfolio/SimpleCRM.png', name: 'Simple CRM', environment: 'Angular | Firebase', text: 'A very Simple Customer Relationship Management system working with CRUD functionality.'}
+    
   ]
   constructor(private translationService: LanguageService) {}
 
